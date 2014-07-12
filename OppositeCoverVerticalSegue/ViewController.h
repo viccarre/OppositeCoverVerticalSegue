c0dbf663c1c86c9be5df48dbbc601b60637fad4c
Victor Carreño <viccarre@victors-mbp.gateway.2wire.net>
2014-07-12 16:22:20 -0500

    Initial Commit

---
+++ OppositeCoverVerticalSegue/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OppositeCoverVerticalSegue
+//
+//  Created by Victor Carreño on 7/12/14.
+//  Copyright (c) 2014 Victor Carreño. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OppositeCoverVerticalSegueTests/OppositeCoverVerticalSegueTests.m
@@ -0,0 +1,34 @@
+//
+//  OppositeCoverVerticalSegueTests.m
+//  OppositeCoverVerticalSegueTests
+//
+//  Created by Victor Carreño on 7/12/14.
+//  Copyright (c) 2014 Victor Carreño. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OppositeCoverVerticalSegueTests : XCTestCase
+
+@end
+
+@implementation OppositeCoverVerticalSegueTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
